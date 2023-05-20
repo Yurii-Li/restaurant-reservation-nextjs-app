2ea821b5f355dea73568c4584b3eb2464d50b4ea
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-05-20 23:51:00 +0300

    Handling of error states (fix)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,17 +3,18 @@
 import Image from "next/image";
 import errorMascot from "../../public/icons/error.png";
 
-export default function Error({error}: { error: Error }) {
+export default function Error({error, reset}: { error: Error, reset: () => void }) {
 
   console.error(error);
 
   return (
     <div className={"h-screen bg-gray-200 flex flex-col justify-center items-center"}>
       <Image src={errorMascot} alt={"error"} className={"w-56 mb-8"}/>
-      <div className={"bg-white px-9 py-14 shadow rounded"}>
+      <div className={"bg-white px-9 py-14 shadow rounded flex flex-col items-center"}>
         <h3 className={"text-3xl font-bold"}>Well, this is embarrassing. Something went wrong.</h3>
-        <p className={"text-reg font-bold"}>{error.message}</p>
-        <p className={"mt-6 text-sm font-light"}>Error Code: 400</p>
+        <button onClick={() => reset()} className={"text-2xl font-bold text-blue-500  hover:text-blue-700"}>
+          Try again
+        </button>
       </div>
     </div>
   )
