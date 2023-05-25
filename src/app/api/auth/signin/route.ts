c49f590a5d2922ad1cb523ce3ed9e75b1a1244f9
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-05-26 01:33:04 +0300

    Implementing the Sign in Endpoint

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,61 @@
+import {NextResponse} from "next/server";
+import validator from 'validator';
+import {PrismaClient} from '@prisma/client'
+import bcrypt from "bcrypt";
+import * as jose from 'jose'
+
+const prisma = new PrismaClient()
+
+export async function POST(req: Request) {
+  const {email, password} = await req.json();
+
+  const errors: string[] = [];
+
+  const validationSchema = [
+    {
+      valid: validator.isEmail(email),
+      errorMessage: 'Email is not valid'
+    },
+    {
+      valid: validator.isLength(password, {min: 1}),
+      errorMessage: 'Password is not valid'
+    }
+  ];
+
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage)
+    }
+  });
+
+  if (errors.length > 0) {
+    return NextResponse.json({errorMessage: errors[0]}, {status: 400})
+  }
+
+  const userWithEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (!userWithEmail) {
+    return NextResponse.json({errorMessage: 'Email or password is not valid'}, {status: 401})
+  }
+
+  const passwordMatch = await bcrypt.compare(password, userWithEmail.password)
+
+  if (!passwordMatch) {
+    return NextResponse.json({errorMessage: 'Email or password is not valid'}, {status: 401})
+  }
+
+  const alg = 'HS256'
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+  const token = await new jose.SignJWT({email: userWithEmail.email})
+    .setProtectedHeader({alg})
+    .setExpirationTime("24h")
+    .sign(secret)
+
+  return NextResponse.json({token}, {status: 200})
+}
