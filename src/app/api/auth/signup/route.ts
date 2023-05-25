fb9187f9753f5950086353e02af11ea4416e3a31
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-05-26 00:52:33 +0300

    Creating and Sending a JSON Web Token

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,6 +2,8 @@ import {NextResponse} from "next/server";
 import validator from 'validator';
 import {PrismaClient} from '@prisma/client'
 import bcrypt from 'bcrypt'
+import * as jose from 'jose'
+
 
 const prisma = new PrismaClient()
 
@@ -73,7 +75,18 @@ export async function POST(req: Request) {
     }
   })
 
-  return NextResponse.json({user}, {status: 200})
+  const alg = 'HS256'
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+  const token = await new jose.SignJWT({email: user.email})
+    .setProtectedHeader({alg})
+    .setExpirationTime("24h")
+    .sign(secret)
+
+  return NextResponse.json({
+    token,
+  }, {status: 200})
 
 }
 
