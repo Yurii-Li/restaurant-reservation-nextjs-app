63cf7d006f0dcb2f88b1521ada3a8accae3da848
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-05-25 19:37:21 +0300

    Creating a User

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -6,7 +6,6 @@ import bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 
 
-
 export async function POST(req: Request) {
 
   const {firstName, lastName, email, phone, city, password} = await req.json()
@@ -15,27 +14,27 @@ export async function POST(req: Request) {
   const validationSchema = [
     {
       valid: validator.isLength(firstName, {min: 1, max: 20}),
-      errorMessage : 'First name must be between 1 and 20 characters'
+      errorMessage: 'First name must be between 1 and 20 characters'
     },
     {
       valid: validator.isLength(lastName, {min: 1, max: 25}),
-      errorMessage : 'Last name must be between 1 and 25 characters'
+      errorMessage: 'Last name must be between 1 and 25 characters'
     },
     {
       valid: validator.isEmail(email),
-      errorMessage : 'Email is not valid'
+      errorMessage: 'Email is not valid'
     },
     {
       valid: validator.isMobilePhone(phone),
-      errorMessage : 'Phone number is not valid'
+      errorMessage: 'Phone number is not valid'
     },
     {
       valid: validator.isLength(city, {min: 1}),
-      errorMessage : 'City is not valid'
+      errorMessage: 'City is not valid'
     },
     {
       valid: validator.isStrongPassword(password),
-      errorMessage : 'Password must be at least 8 characters long and contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol'
+      errorMessage: 'Password must be at least 8 characters long and contain at least 1 lowercase, 1 uppercase, 1 number and 1 symbol'
     }
   ]
 
@@ -48,7 +47,7 @@ export async function POST(req: Request) {
   })
 
   if (errors.length > 0) {
-    return NextResponse.json({errorMessage : errors[0]}, {status: 400})
+    return NextResponse.json({errorMessage: errors[0]}, {status: 400})
   }
 
   const userWithEmail = await prisma.user.findUnique({
@@ -58,11 +57,23 @@ export async function POST(req: Request) {
   })
 
   if (userWithEmail) {
-    return NextResponse.json({errorMessage : 'Email is associated with another account'}, {status: 400})
+    return NextResponse.json({errorMessage: 'Email is associated with another account'}, {status: 400})
   }
 
   const hashedPassword = await bcrypt.hash(password, 10)
 
+  const user = await prisma.user.create({
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      phone,
+      city,
+      password: hashedPassword
+    }
+  })
+
+  return NextResponse.json({user}, {status: 200})
 
 }
 
