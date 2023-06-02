6c57aaaa9fc9c22df9487e1cddfb6be011aadd14
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-06-02 20:28:11 +0300

    Implementation of values for global state

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,27 @@
 import axios from "axios";
+import {AuthenticationContext} from "@/app/context/AuthContext";
+import {useContext} from "react";
 
 const useAuth = () => {
 
+  const {data, loading, error, setAuthState} = useContext(AuthenticationContext)
+
   const signin = async ({email, password}: { email: string, password: string }) => {
+
+    setAuthState({loading: true, data: null, error: null})
 
     try {
       const response = await axios.post('/api/auth/signin', {email, password})
 
-      console.log(response.data)
-
+      setAuthState({loading: false, data: response.data, error: null})
     } catch (e) {
-      console.log(e)
+      if (axios.isAxiosError(e) && e.response) {
+        setAuthState({loading: false, data: null, error: e.response.data.errorMessage})
+      } else {
+        setAuthState({loading: false, data: null, error: String(e)})
+      }
     }
+
   }
 
   const signup = async () => {
