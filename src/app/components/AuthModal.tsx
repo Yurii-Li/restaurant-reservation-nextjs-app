dd7e041b6555370a6b794bb9a1bacf1349ce69f5
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-06-02 20:52:46 +0300

    Handling of loading state and error state

--- src/app/components/AuthModal.tsx
+++ src/app/components/AuthModal.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import {ChangeEvent, useEffect, useState} from "react";
+import {ChangeEvent, useContext, useEffect, useState} from "react";
 import AuthModalInputs from "@/app/components/AuthModalInputs";
 import useAuth from "@/hooks/useAuth";
+import {AuthenticationContext} from "@/app/context/AuthContext";
 
 export default function AuthModal({isSignin}: { isSignin: boolean }) {
 
@@ -36,7 +37,10 @@ export default function AuthModal({isSignin}: { isSignin: boolean }) {
   }, [inputs])
 
 
-  const {signin, signup} = useAuth();
+  const {signin} = useAuth();
+
+  const {error, loading,} = useContext(AuthenticationContext);
+
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputs({
@@ -49,11 +53,14 @@ export default function AuthModal({isSignin}: { isSignin: boolean }) {
     setModal(!modal)
   }
 
-  if (modal) {
-    document.body.classList.add("overflow-hidden")
-  } else {
-    document.body.classList.remove("overflow-hidden")
-  }
+  useEffect(() => {
+    if (modal) {
+      document.body.classList.add("overflow-hidden")
+    } else {
+      document.body.classList.remove("overflow-hidden")
+    }
+  }, [modal])
+
 
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignin ? signinContent : signupContent
@@ -78,7 +85,7 @@ export default function AuthModal({isSignin}: { isSignin: boolean }) {
           <div onClick={toggleModal}
                className={"w-screen h-screen top-0 right-0 bottom-0 left-0 fixed bg-gray-950/80"}></div>
           <div
-            className={"absolute top-[50%] left-[50%]  -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded max-w-2xl min-w-[300px] "}>
+            className={" absolute top-[50%] left-[50%]  -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded max-w-2xl min-w-[300px] "}>
 
             <div className={"p-2"}>
               <div className={"uppercase font-bold text-center pb-2 border-b mb-2"}>
@@ -94,12 +101,28 @@ export default function AuthModal({isSignin}: { isSignin: boolean }) {
 
                 <AuthModalInputs inputs={inputs} handleChangeInput={handleChangeInput} isSignin={isSignin}/>
 
-                <button disabled={disabled}
-                        className={"uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"}
-                        onClick={handleClick}
-                >
-                  {renderContent("Sign In", "Sign Up")}
-                </button>
+                {
+                  loading ? (
+                    <div className={" flex justify-center items-center"}>
+                      <div
+                        className="w-12 h-12 border-[5px] border-b-red-600 rounded-full inline-block animate-spin"></div>
+
+                    </div>
+                  ) : (
+                    <button disabled={disabled}
+                            className={"uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400"}
+                            onClick={handleClick}
+                    >
+                      {renderContent("Sign In", "Sign Up")}
+                    </button>
+                  )
+                }
+
+                {
+                  error && (
+                    <div className={"bg-red-500 text-white text-sm p-2 rounded  mb-5"}>!!! {error}</div>
+                  )
+                }
 
               </div>
 
