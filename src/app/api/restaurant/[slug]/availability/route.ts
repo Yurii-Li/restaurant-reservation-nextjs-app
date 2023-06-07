d3c8d362df76ad58b3f5497ee59cdcdde7d332ae
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-06-07 21:32:48 +0300

    Fetching the Bookings, Compressing the Booking, Fetching the Restaurant Tables

--- src/app/api/restaurant/[slug]/availability/route.ts
+++ src/app/api/restaurant/[slug]/availability/route.ts
@@ -1,5 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
-import { times } from "@/data/times";
+import {NextRequest, NextResponse} from "next/server";
+import {times} from "@/data/times";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   const slug = req.nextUrl.pathname.split("/")[3];
@@ -9,8 +12,8 @@ export async function GET(req: NextRequest) {
 
   if (!day || !time || !partySize) {
     return NextResponse.json(
-      { errorMessage: "Invalid data provided" },
-      { status: 400 }
+      {errorMessage: "Invalid data provided"},
+      {status: 400}
     );
   }
 
@@ -18,17 +21,69 @@ export async function GET(req: NextRequest) {
     return t.time === time;
   })?.searchTimes;
 
+  console.log("searchTimes", searchTimes)
+
   if (!searchTimes) {
     return NextResponse.json(
-      { errorMessage: "Invalid data provided" },
-      { status: 400 }
+      {errorMessage: "Invalid data provided"},
+      {status: 400}
     );
   }
 
-  return NextResponse.json({ searchTimes }, { status: 200 });
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  const bookingTablesObj: { [key: string]: { [key: number]: true } } = {};
+
+  bookings.forEach((booking) => {
+    bookingTablesObj[booking.booking_time.toISOString()] =
+      booking.tables.reduce((acc, table) => {
+        return {
+          ...acc,
+          [table.table_id]: true,
+        };
+      }, {});
+  });
+
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true
+    }
+  });
+
+  if (!restaurant) {
+    return NextResponse.json(
+      {errorMessage: "Invalid data provided"},
+      {status: 400}
+    );
+  }
+
+  const tables = restaurant.tables
+
+
+
+  return NextResponse.json(
+    {searchTimes, bookings, bookingTablesObj, tables},
+    {status: 200}
+  );
 }
 
-//http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-02-03&time=15:00:00.000Z&partySize=8
+//http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-06-07&time=14:00:00.000Z&partySize=8
 
 // ПОМИЛКИ: HTTP ERROR 500. req.query = undefined  В НАСЛІДОК ЦЬОГО TypeError: Cannot destructure property 'slug' of 'req.query' as it is undefined.
 // export async function GET(req: NextApiRequest) {
