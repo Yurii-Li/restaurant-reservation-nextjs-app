c0b0a1b9594b07b0f63015f508c3666be9a3d758
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-06-07 18:07:02 +0300

    Building the Availability Endpoint. Issues with a better endpoint creation solution

---
+++ src/app/api/restaurant/[slug]/availability/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import {times} from "@/data/times";
+
+export async function GET(req: NextRequest) {
+  const slug = req.nextUrl.pathname.split("/")[3];
+  const day = req.nextUrl.searchParams.get("day");
+  const time = req.nextUrl.searchParams.get("time");
+  const partySize = req.nextUrl.searchParams.get("partySize");
+
+  if (!day || !time || !partySize) {
+    return NextResponse.json(
+      { errorMessage: "Invalid data provided" },
+      { status: 400 }
+    );
+  }
+
+
+  return NextResponse.json({ slug, day, time, partySize }, { status: 200 });
+}
+
+//http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-02-03&time=15:00:00.000Z&partySize=8
+
+// ПОМИЛКИ: HTTP ERROR 500. req.query = undefined  В НАСЛІДОК ЦЬОГО TypeError: Cannot destructure property 'slug' of 'req.query' as it is undefined.
+// export async function GET(req: NextApiRequest) {
+//
+//   // Чому req.query повертає undefined?
+//   console.log("req.query", req.query );
+//
+//   const {slug, day, time, partySize} = req.query as {slug: string, day: string, time: string, partySize: string};
+//
+//   if (!day || !time || !partySize) {
+//     return NextResponse.json({ errorMessage: "Invalid data provided" }, { status: 400 });
+//   }
+//
+//   return NextResponse.json( { slug, day, time, partySize }, { status: 200 });
+// }
