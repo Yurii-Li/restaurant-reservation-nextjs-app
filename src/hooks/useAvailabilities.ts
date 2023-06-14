6bc919ef11bfce30f367a7f349f3e32c7a7c509d
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-06-14 15:33:08 +0300

    useAvailabilities

---
+++ src/hooks/useAvailabilities.ts
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function useAvailabilities() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    day,
+    time,
+    partySize,
+  }: {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: string;
+  }) => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`api/restaurant/${slug}/availability`, {
+        params: {
+          day,
+          time,
+          partySize,
+        },
+      });
+
+      setData(response.data);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      if (axios.isAxiosError(e) && e.response) {
+        setError(e.response.data.errorMessage);
+      } else {
+        setError(String(e));
+      }
+    }
+  };
+
+  return {
+    loading,
+    error,
+    data,
+    fetchAvailabilities,
+  };
+}
