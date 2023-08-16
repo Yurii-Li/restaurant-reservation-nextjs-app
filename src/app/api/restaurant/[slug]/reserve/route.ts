6489ce2718a238746476c30db0bdaed56a1e6638
Yurii Liubuskyi <liubuskiy.yuri@gmail.com>
2023-08-16 13:17:04 +0300

    1. Count the Tables Based on Seats 2. Determine the Tables to Book

--- src/app/api/restaurant/[slug]/reserve/route.ts
+++ src/app/api/restaurant/[slug]/reserve/route.ts
@@ -66,5 +66,48 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  return NextResponse.json(searchTimeWithTables, { status: 200 });
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  const tablesToBooks: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBooks.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBooks.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
+  return NextResponse.json(tablesToBooks, { status: 200 });
 }
